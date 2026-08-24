5dcf17391cdd48e13ffc3f827f954821ac9a84a8
brunolucas2 <galaxyslayer0009@gmail.com>
2026-08-24 20:17:18 -0300

    contagem-de-tentativas.c

--- estruturas-de-controle/loops/contagem-de-tentativas.c
+++ estruturas-de-controle/loops/contagem-de-tentativas.c
@@ -4,7 +4,7 @@ int main()
 {
     int senha = 1234, input, count = 1;
 
-    while (5)
+    while (count < 6)
     {
         printf("Digite a senha: ");
         scanf("%i", &input);
@@ -15,7 +15,13 @@ int main()
             return 0;
         }
 
+        if (count == 5) {
+            printf("Acesso negado!");
+            return 0;
+        }
+        
         printf("Senha incorreta.\nRestam %i tentativas.", 5 - count);
+        count++;
         printf("\n");
     }
 }
