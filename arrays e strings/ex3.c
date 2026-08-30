f847df1dc19c7868a596fba18fa3ab3fe50b3918
bruno <galaxyslayer0009@gmail.com>
2026-08-29 23:38:42 -0300

    3 - exercicio, utilizando matriz

--- arrays e strings/ex3.c
+++ arrays e strings/ex3.c
@@ -1,18 +1,27 @@
+#include <stdio.h>
+
 int main()
 {
-    int testes[4][10] = {
-        {-5, 10, 3, -2, 8, -7, 4, 0, -1, 6},
-        {-8, -4, 0, -2, -10, -1, -7, -3, -6, -9},
-        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-    };
-
-    for (int i = 0; i < sizeof(testes) / sizeof(testes[0]))
+    int testes[4][6] = {
+        {1, 2, 3, 4, 5, 6},
+        {10, 20, 30, 40, 50, 60},
+        {7, 7, 7, 7, 7, 7},
+        {-1, -2, -3, -4, -5, -6}};
+    int left, right;
+    for (int i = 0; i < sizeof(testes) / sizeof(testes[0]); i++)
     {
-        int j = 0;
-        
-        for
+        int last = testes[i][sizeof(testes[0]) / sizeof(testes[0][0]) - 1];
+        for (int j = sizeof(testes[0]) / sizeof(testes[0][0]) - 1; j > 0; j--)
+        {
+            testes[i][j] = testes[i][j - 1];
+        }
+        testes[i][0] = last;
+        printf("Teste[%i]:", i);
+        for (int k = 0; k < sizeof(testes[0]) / sizeof(testes[0][0]); k++)
+        {
+            printf(" %i ", testes[i][k]);
+        }
+        printf("\n");
     }
-
     return 0;
 }
