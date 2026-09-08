ca49172a5001ce74a0ff7c9189858b271c013035
bruno <galaxyslayer0009@gmail.com>
2026-09-07 23:54:16 -0300

    exercicio 8, arrays e strings com for

--- arrays e strings/ex8.c
+++ arrays e strings/ex8.c
@@ -2,5 +2,32 @@
 
 int main()
 {
-    printf("exercicio 7")
+    char str[50];
+    char vogais[5] = {'a', 'e', 'i', 'o', 'u'};
+
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; i < 5; i++)
+    {
+        int contador_de_vezes = 0;
+
+        for (int j = 0; str[j] != '\0'; j++)
+        {
+            if (str[j] == vogais[i])
+            {
+                contador_de_vezes++;
+            }
+        }
+
+        printf("%i", contador_de_vezes);
+
+        if (i < 4)
+        {
+            printf(" ");
+        }
+    }
+
+    printf("\n");
+
+    return 0;
 }
