328a178abcc1f7dc784e06a0c485cbb191a5f98a
bruno <galaxyslayer0009@gmail.com>
2026-09-07 23:55:02 -0300

    exercicio 5 comentado

--- arrays e strings/ex8.c
+++ arrays e strings/ex8.c
@@ -5,12 +5,15 @@ int main()
     char str[50];
     char vogais[5] = {'a', 'e', 'i', 'o', 'u'};
 
+    // Lê a frase digitada pelo usuário
     fgets(str, sizeof(str), stdin);
 
+    // Percorre cada uma das 5 vogais
     for (int i = 0; i < 5; i++)
     {
         int contador_de_vezes = 0;
 
+        // Percorre a frase contando quantas vezes a vogal aparece
         for (int j = 0; str[j] != '\0'; j++)
         {
             if (str[j] == vogais[i])
